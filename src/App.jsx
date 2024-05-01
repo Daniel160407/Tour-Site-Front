7c694d99545c2a85dd9cc34228a3515163fdb931
Daniel160407 <abulashvilidani@hotmail.com>
2024-05-01 20:45:08 +0400

    Added not found page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import AdminPanel from './components/adminpanel/AdminPanel';
 import Tours from './components/Tours';
+import NotFound from './components/NotFound';
 
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
             <Switch>
                 <Route path="/tours/adminpanel" component={AdminPanel}/>
                 <Route path="/tours" component={Tours}/>
+                <Route component={NotFound}/>
             </Switch>
         </Router>
     );
