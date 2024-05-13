95f3b6357eae731166cf8704a6371e703bd4c624
Daniel160407 <abulashvilidani@hotmail.com>
2024-05-14 00:10:40 +0400

    Added offer card

--- src/components/tour/Home.jsx
+++ src/components/tour/Home.jsx
@@ -1,16 +1,19 @@
-import React, {useEffect, useState} from "react";
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import '../../style/tour/home.scss';
 import Tour from "../Tour";
 import EditTourForm from "../adminpanel/tour/EditTourForm";
 import AddTour from "../adminpanel/tour/AddTour";
 
-function Home({adminMode}) {
+// eslint-disable-next-line react/prop-types
+function Home({ adminMode }) {
     const [tours, setTours] = useState([]);
     const [selectedTour, setSelectedTour] = useState(null);
     const [showTour, setShowTour] = useState(true);
     const [addTour, setAddTour] = useState(false);
     const [language, setLanguage] = useState('ENG');
+    const [randomTour, setRandomTour] = useState(null);
 
     useEffect(() => {
         if (language) {
@@ -19,9 +22,10 @@ function Home({adminMode}) {
                     const toursWithImages = response.data.tours.map(tour => {
                         const byteCharacters = atob(tour.imageData);
                         const base64String = btoa(byteCharacters);
-                        return {...tour, imageData: base64String};
+                        return { ...tour, imageData: base64String };
                     });
                     setTours(toursWithImages);
+                    setRandomTour(Math.floor(Math.random() * toursWithImages.length));
                 })
                 .catch(error => {
                     console.error(error);
@@ -65,8 +69,7 @@ function Home({adminMode}) {
                                     <div key={tour.name} className="tour" onClick={() => openTour(tour)}>
                                         <div className="header">
                                             <div>
-                                                <img src={`data:image/jpeg;base64,${tour.imageData}`}
-                                                     alt={tour.name}></img>
+                                                <img src={`data:image/jpeg;base64,${tour.imageData}`} alt={tour.name}></img>
                                             </div>
                                             <div className="titleBox">
                                                 <h1 className="title">{tour.name}</h1>
@@ -79,13 +82,36 @@ function Home({adminMode}) {
                                             <p>{tour.description}</p>
                                         </div>
                                         <div className="price">
-                                            <h3>From: {tour.price}</h3>
+                                            <h3>{language === 'ENG' ? 'From' : 'От'} {tour.price}</h3>
                                         </div>
                                     </div>
                                 ))}
                             </div>
                         )}
                     </div>
+                    {randomTour !== null && (
+                        <div className="card">
+                            <div className="container-image">
+                                <img className="image-circle" src={`data:image/jpeg;base64,${tours[randomTour].imageData}`}></img>
+                                <h2>{tours[randomTour].name}</h2>
+                            </div>
+                            <div className="content">
+                                <div className="detail">
+                                    <span>{tours[randomTour].name}</span>
+                                    <p>{tours[randomTour].price}</p>
+                                    <div className="buttons">
+                                        <button onClick={() => {openTour(tours[randomTour])}}>Info</button>
+                                        <button onClick={() => {setRandomTour(null)}}>Close</button>
+                                    </div>
+                                </div>
+                                <div className="product-image">
+                                    <div className="box-image">
+                                        <img className="img-product" src={`data:image/jpeg;base64,${tours[randomTour].imageData}`}></img>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
             )}
             {!showTour && addTour && (
