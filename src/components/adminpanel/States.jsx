2d26319fb12336ec0bcd29d8827ec80d4a95e679
Daniel160407 <abulashvilidani@hotmail.com>
2024-05-27 15:42:22 +0400

    Added ability to clear states

--- src/components/adminpanel/States.jsx
+++ src/components/adminpanel/States.jsx
@@ -27,24 +27,39 @@ const States = () => {
         return () => clearInterval(intervalId);
     }, []);
 
+    const clearState = (state) => {
+        axios.delete(`http://localhost:8080/states?state=${state}`)
+        .then(response => {
+            const data = response.data;
+            setTime(data.time);
+            setClicks(data.clicks);
+            setMessages(data.messages);
+            setFeedbacks(data.feedbacks);
+        });
+    }
+
     return (
         <div id='states' className='tab-pane tab fade'>
             <div className='states-container'>
                 <div className='state-item'>
                     <h2>Time Spent</h2>
                     <p>{time} minutes</p>
+                    <button className='clear-button' onClick={() => clearState('time')}>Clear</button>
                 </div>
                 <div className='state-item'>
                     <h2>Clicks</h2>
                     <p>{clicks}</p>
+                    <button className='clear-button' onClick={() => clearState('clicks')}>Clear</button>
                 </div>
                 <div className='state-item'>
                     <h2>Messages</h2>
                     <p>{messages}</p>
+                    <button className='clear-button' onClick={() => clearState('messages')}>Clear</button>
                 </div>
                 <div className='state-item'>
                     <h2>Feedbacks</h2>
                     <p>{feedbacks}</p>
+                    <button className='clear-button' onClick={() => clearState('feedbacks')}>Clear</button>
                 </div>
             </div>
         </div>
